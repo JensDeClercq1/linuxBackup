563072fdbe4b49c8a52822594253014426168eae
JensDeClercq1 <127455327+JensDeClercq1@users.noreply.github.com>
2023-12-10 02:02:33 +0100

    Add files via upload

--- dataPull.sh
+++ dataPull.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#Crypto currencies config
 api_key="44CD512C-79C1-4A5F-A6E0-5814C1D2E0F1"
 currentPath=$(pwd)
-output_dir="${currentPath}/cryptoData/"
+output_dir="${currentPath}/pulledData/"
 base_url="https://rest.coinapi.io/v1/exchangerate/"
 
 if [ ! -d "${output_dir}" ]; then
@@ -41,3 +42,28 @@ get_data(){
 get_data "BTC/EUR"
 get_data "BTC/ETH"
 get_data "ETH/EUR"
+
+#Goud config
+goud_url="https://www.goldapi.io/api/XAU/EUR" 
+goud_api_key="goldapi-pehd1rlpyj9ww8-io"
+
+get_gold_data(){
+  local tijd
+  tijd=$(date +"%Y%m%d-%H%M%S")
+  local data_file="${output_dir}data-${tijd}.json"
+  local error_file="${output_dir}error-${tijd}.json"
+  curl_pull() {
+    curl -X GET "${goud_url}" -H "x-access-token: ${goud_api_key}" >"${data_file}" 2>"${error_file}"
+  }
+
+  if curl_pull; then
+    rm -f "${error_file}"
+    echo "De goudprijs is opgehaald en opgeslagen in ${data_file}."
+  else
+    rm -f "${data_file}"
+    echo "Er is een fout opgetreden bij de Goud API-oproep."
+    echo "Bekijk het foutbestand voor meer informatie: ${error_file}."
+  fi
+}
+
+get_gold_data
--- dataTransfer.sh
+++ dataTransfer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-input_dir="/home/jensd/cryptoData/"
+input_dir="/home/jensd/pulledData/"
 pad="/home/jensd/csvfiles"
 if [ ! -d "${pad}" ]; then
     mkdir "${pad}"
@@ -7,18 +7,32 @@ fi
 
 
 for json_file in "$input_dir"/*.json; do
-    tijd=$(jq -r '.time' "$json_file")
-    van=$(jq -r '.asset_id_base' "$json_file")
-    naar=$(jq -r '.asset_id_quote' "$json_file")
-    ER=$(jq -r '.rate' "$json_file")
+    if jq -e 'has("asset_id_base") and has("asset_id_quote")' "$json_file" > /dev/null; then
+        tijd=$(jq -r '.time' "$json_file")
+        van=$(jq -r '.asset_id_base' "$json_file")
+        naar=$(jq -r '.asset_id_quote' "$json_file")
+        ER=$(jq -r '.rate' "$json_file")
 
-    output_csv="${pad}/${van}_${naar}.csv"
-    mv "${json_file}" "${json_file}.txt"
+        output_csv="${pad}/${van}_${naar}.csv"
+        mv "${json_file}" "${json_file}.txt"
     
-    if [ ! -s "$output_csv" ];then
-        echo "Tijd,Van,Naar,Exchange Rate" > "$output_csv"
+        if [ ! -s "$output_csv" ];then
+            echo "Tijd,Van,Naar,Exchange Rate" > "$output_csv"
+        fi
+        echo "$tijd,$van,$naar,$ER" >> "$output_csv"
+        else
+        # Goudgegevens
+        goud_tijd=$(jq -r '.timestamp' "$json_file")
+        goud_prijs=$(jq -r '.price' "$json_file")
+
+        output_csv="${pad}/gold.csv"
+        mv "${json_file}" "${json_file}.txt"
+
+        if [ ! -s "$output_csv" ]; then
+            echo "Tijd,Prijs" > "$output_csv"
+        fi
+        echo "$goud_tijd,$goud_prijs" >> "$output_csv"
     fi
-    echo "$tijd,$van,$naar,$ER" >> "$output_csv"
 done
 
 echo "Transformatie voltooid en opgeslagen in ${pad}."
